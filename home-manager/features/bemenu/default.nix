efd9d057e8893d91b7113e0a1f19e11129f07406
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2024-03-18 23:04:06 -0500

    fix duplicate directory

--- home-manager/features/bemenu/default.nix
+++ home-manager/features/bemenu/default.nix
@@ -4,7 +4,7 @@
     j4-dmenu-desktop
   ];
 
-  wayland.windowManager.hyprland.settings.bind = [ "SUPER, R, exec, ${pkgs.j4-dmenu-desktop}/bin/j4-dmenu-desktop --no-generic --dmenu=bemenu" ];
+  wayland.windowManager.hyprland.settings.bind = [ "SUPER, D, exec, ${pkgs.j4-dmenu-desktop}/bin/j4-dmenu-desktop --no-generic --dmenu=bemenu" ];
 
   programs.bemenu = {
     enable = true;
--- home-manager/features/bemenu/zoxide/default.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ zoxide ];
-  programs.zoxide = {
-    enable = true;
-  };
-}
