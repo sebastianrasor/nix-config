493f6afd52fe68c0651c0c765c1b9192f92d473f
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2024-04-17 08:11:10 -0500

    move dtach fork to its own repo

--- pkgs/dtach/default.nix
+++ pkgs/dtach/default.nix
@@ -1,9 +1,14 @@
-{ stdenv }:
+{ fetchFromGitHub, stdenv }:
 stdenv.mkDerivation {
   pname = "dtach";
   version = "0.10.0";
 
-  src = ./.;
+  src = fetchFromGitHub {
+    owner = "sebastianrasor";
+    repo = "dtach";
+    rev = "f3dc5886992a5544fe7412565b4ce4a47d019a19";
+    hash = "sha256-eZWpP2IYwTKlKXx8Wnmau1B1jKb4qdIWjTiD77/to0Y=";
+  };
 
   buildPhase = ''
     ./configure && make
