ec853dd7248a2222cfc839de7b5d9a56ad05684a
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2024-05-02 07:48:13 -0500

    nixos graphics stuff

--- nixos/framework/configuration.nix
+++ nixos/framework/configuration.nix
@@ -150,7 +150,23 @@
     };
   };
 
-  boot.kernelParams = [ "mem_sleep_default=deep" ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Force intel-media-driver
+
+  boot.kernelParams = [
+    "i915.force_probe=9a49"
+    "mem_sleep_default=deep"
+  ];
   services.logind = {
     lidSwitch = "suspend-then-hibernate";
     extraConfig = ''
