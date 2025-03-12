89bcbe59ed730b517a7b39142fc3ef9fcc556d5d
Sebastian Rasor <git@rasor.us>
2025-03-12 17:42:29 -0500

    feat: improve neovim indentation and formatting behavior

--- modules/home-manager/neovim/init.lua
+++ modules/home-manager/neovim/init.lua
@@ -1,8 +1,29 @@
 require("hardtime").setup()
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "nixfmt", lsp_format = "fallback" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
 
 vim.opt.termguicolors = false
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.cindent = true
+vim.opt.scrolloff = 4
+vim.opt.undofile = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.vb = true
 
 function _G.lsp_status()
 	local lspStatus = vim.lsp.status()
@@ -13,44 +34,56 @@ function _G.lsp_status()
 	end
 end
 
-if vim.fn.executable('rust-analyzer') == 1 then
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'rust',
-    callback = function(args)
-      vim.lsp.start({
-        name = 'rust-analyzer',
-        cmd = {'rust-analyzer'},
-        root_dir = vim.fs.root(args.buf, {'Cargo.toml'}),
-      })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "nix",
+	callback = function()
+		vim.opt.scrolloff = 2
+		vim.opt.undofile = true
+		vim.opt.shiftwidth = 2
+		vim.opt.softtabstop = 2
+		vim.opt.tabstop = 2
+		vim.opt.expandtab = true
+	end,
+})
+
+if vim.fn.executable("rust-analyzer") == 1 then
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "rust",
+		callback = function(args)
+			vim.lsp.start({
+				name = "rust-analyzer",
+				cmd = { "rust-analyzer" },
+				root_dir = vim.fs.root(args.buf, { "Cargo.toml" }),
+			})
 			vim.opt.statusline = [[%!v:lua.lsp_status()]]
-    end,
-  })
+		end,
+	})
 end
 
-if vim.fn.executable('nixd') == 1 then
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'nix',
-    callback = function(args)
-      vim.lsp.start({
-        name = 'nixd',
-        cmd = {'nixd'},
-        --root_dir = vim.fs.root(args.buf, {'flake.nix'}),
-      })
+if vim.fn.executable("nixd") == 1 then
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "nix",
+		callback = function(args)
+			vim.lsp.start({
+				name = "nixd",
+				cmd = { "nixd" },
+				--root_dir = vim.fs.root(args.buf, {'flake.nix'}),
+			})
 			vim.opt.statusline = [[%!v:lua.lsp_status()]]
-    end,
-  })
-elseif vim.fn.executable('nil') == 1 then
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'nix',
-    callback = function(args)
-      vim.lsp.start({
-        name = 'nil',
-        cmd = {'nil'},
-        root_dir = vim.fs.root(args.buf, {'flake.nix'}),
-      })
+		end,
+	})
+elseif vim.fn.executable("nil") == 1 then
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "nix",
+		callback = function(args)
+			vim.lsp.start({
+				name = "nil",
+				cmd = { "nil" },
+				root_dir = vim.fs.root(args.buf, { "flake.nix" }),
+			})
 			vim.opt.statusline = [[%!v:lua.lsp_status()]]
-    end,
-  })
+		end,
+	})
 end
 
 --
@@ -72,133 +105,136 @@ end
 --  end
 --})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'Enable vim.lsp.completion',
-  callback = function(event)
-    local client_id = vim.tbl_get(event, 'data', 'client_id')
-    if client_id == nil then
-      return
-    end
-    local client = vim.lsp.get_client_by_id(client_id)
-    ---Utility for keymap creation.
-    ---@param lhs string
-    ---@param rhs string|function
-    ---@param opts string|table
-    ---@param mode? string|string[]
-    local function keymap(lhs, rhs, opts, mode)
-        opts = type(opts) == 'string' and { desc = opts }
-            or vim.tbl_extend('error', opts --[[@as table]], { buffer = event.buf })
-        mode = mode or 'n'
-        vim.keymap.set(mode, lhs, rhs, opts)
-    end
-    
-    ---For replacing certain <C-x>... keymaps.
-    ---@param keys string
-    local function feedkeys(keys)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), 'n', true)
-    end
-    
-    ---Is the completion menu open?
-    local function pumvisible()
-        return tonumber(vim.fn.pumvisible()) ~= 0
-    end
-    
-    -- Enable completion and configure keybindings.
-    vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
-    
-    -- Use enter to accept completions.
-    keymap('<cr>', function()
-        return pumvisible() and '<C-y>' or '<cr>'
-    end, { expr = true }, 'i')
-    
-    -- Use slash to dismiss the completion menu.
-    keymap('/', function()
-        return pumvisible() and '<C-e>' or '/'
-    end, { expr = true }, 'i')
-    
-    -- Use <C-n> to navigate to the next completion or:
-    -- - Trigger LSP completion.
-    -- - If there's no one, fallback to vanilla omnifunc.
-    keymap('<C-n>', function()
-        if pumvisible() then
-            feedkeys '<C-n>'
-        else
-            if next(vim.lsp.get_clients { bufnr = 0 }) then
-                vim.lsp.completion.trigger()
-            else
-                if vim.bo.omnifunc == '' then
-                    feedkeys '<C-x><C-n>'
-                else
-                    feedkeys '<C-x><C-o>'
-                end
-            end
-        end
-    end, 'Trigger/select next completion', 'i')
-    
-    -- Buffer completions.
-    keymap('<C-u>', '<C-x><C-n>', { desc = 'Buffer completions' }, 'i')
-    
-    -- Use <Tab> to accept a Copilot suggestion, navigate between snippet tabstops,
-    -- or select the next completion.
-    -- Do something similar with <S-Tab>.
-    keymap('<Tab>', function()
-        if pumvisible() then
-            feedkeys '<C-n>'
-        elseif vim.snippet.active { direction = 1 } then
-            vim.snippet.jump(1)
-        else
-            feedkeys '<Tab>'
-        end
-    end, {}, { 'i', 's' })
-    keymap('<S-Tab>', function()
-        if pumvisible() then
-            feedkeys '<C-p>'
-        elseif vim.snippet.active { direction = -1 } then
-            vim.snippet.jump(-1)
-        else
-            feedkeys '<S-Tab>'
-        end
-    end, {}, { 'i', 's' })
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "Enable vim.lsp.completion",
+	callback = function(event)
+		local client_id = vim.tbl_get(event, "data", "client_id")
+		if client_id == nil then
+			return
+		end
+		local client = vim.lsp.get_client_by_id(client_id)
+		---Utility for keymap creation.
+		---@param lhs string
+		---@param rhs string|function
+		---@param opts string|table
+		---@param mode? string|string[]
+		local function keymap(lhs, rhs, opts, mode)
+			opts = type(opts) == "string" and { desc = opts }
+				or vim.tbl_extend("error", opts --[[@as table]], { buffer = event.buf })
+			mode = mode or "n"
+			vim.keymap.set(mode, lhs, rhs, opts)
+		end
 
-    vim.opt.completeopt = {'menu','noselect', 'fuzzy'}
-    vim.o.foldenable = true
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldmethod = "expr"
-    vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
-    
-    -- Inside a snippet, use backspace to remove the placeholder.
-    keymap('<BS>', '<C-o>s', {}, 's')
+		---For replacing certain <C-x>... keymaps.
+		---@param keys string
+		local function feedkeys(keys)
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), "n", true)
+		end
 
-		keymap('[g', vim.diagnostic.goto_prev, {})
-		keymap(']g', vim.diagnostic.goto_next, {})
-		keymap('gd', vim.lsp.buf.definition, {})
-		keymap('gD', vim.lsp.buf.declaration, {})
-		keymap('gr', vim.lsp.buf.references, {})
-		keymap('gt', vim.lsp.buf.type_definition, {})
-		keymap('gi', vim.lsp.buf.implementation, {})
-		keymap('K', vim.lsp.buf.hover, {})
-		keymap('<C-k>', vim.lsp.buf.signature_help, {})
-		keymap('<Leader>rn', vim.lsp.buf.rename, {})
-		keymap('<Leader>ca', vim.lsp.buf.code_action, {}, { 'n', 'v' })
-		keymap('<Leader>f', function()
-			vim.lsp.buf.format { async = true }
+		---Is the completion menu open?
+		local function pumvisible()
+			return tonumber(vim.fn.pumvisible()) ~= 0
+		end
+
+		-- Enable completion and configure keybindings.
+		vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = true })
+
+		-- Use enter to accept completions.
+		keymap("<cr>", function()
+			return pumvisible() and "<C-y>" or "<cr>"
+		end, { expr = true }, "i")
+
+		-- Use slash to dismiss the completion menu.
+		keymap("/", function()
+			return pumvisible() and "<C-e>" or "/"
+		end, { expr = true }, "i")
+
+		-- Use <C-n> to navigate to the next completion or:
+		-- - Trigger LSP completion.
+		-- - If there's no one, fallback to vanilla omnifunc.
+		keymap("<C-n>", function()
+			if pumvisible() then
+				feedkeys("<C-n>")
+			else
+				if next(vim.lsp.get_clients({ bufnr = 0 })) then
+					vim.lsp.completion.trigger()
+				else
+					if vim.bo.omnifunc == "" then
+						feedkeys("<C-x><C-n>")
+					else
+						feedkeys("<C-x><C-o>")
+					end
+				end
+			end
+		end, "Trigger/select next completion", "i")
+
+		-- Buffer completions.
+		keymap("<C-u>", "<C-x><C-n>", { desc = "Buffer completions" }, "i")
+
+		-- Use <Tab> to accept a Copilot suggestion, navigate between snippet tabstops,
+		-- or select the next completion.
+		-- Do something similar with <S-Tab>.
+		keymap("<Tab>", function()
+			if pumvisible() then
+				feedkeys("<C-n>")
+			elseif vim.snippet.active({ direction = 1 }) then
+				vim.snippet.jump(1)
+			else
+				feedkeys("<Tab>")
+			end
+		end, {}, { "i", "s" })
+		keymap("<S-Tab>", function()
+			if pumvisible() then
+				feedkeys("<C-p>")
+			elseif vim.snippet.active({ direction = -1 }) then
+				vim.snippet.jump(-1)
+			else
+				feedkeys("<S-Tab>")
+			end
+		end, {}, { "i", "s" })
+
+		vim.opt.completeopt = { "menu", "noselect", "fuzzy" }
+		vim.o.foldenable = true
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldmethod = "expr"
+		vim.o.foldexpr = "v:lua.vim.lsp.foldexpr()"
+
+		-- Inside a snippet, use backspace to remove the placeholder.
+		keymap("<BS>", "<C-o>s", {}, "s")
+
+		keymap("[g", vim.diagnostic.goto_prev, {})
+		keymap("]g", vim.diagnostic.goto_next, {})
+		keymap("gd", vim.lsp.buf.definition, {})
+		keymap("gD", vim.lsp.buf.declaration, {})
+		keymap("gr", vim.lsp.buf.references, {})
+		keymap("gt", vim.lsp.buf.type_definition, {})
+		keymap("gi", vim.lsp.buf.implementation, {})
+		keymap("K", vim.lsp.buf.hover, {})
+		keymap("<C-k>", vim.lsp.buf.signature_help, {})
+		keymap("<Leader>rn", vim.lsp.buf.rename, {})
+		keymap("<Leader>ca", vim.lsp.buf.code_action, {}, { "n", "v" })
+		keymap("<Leader>f", function()
+			vim.lsp.buf.format({ async = true })
 		end, {})
-  end
+	end,
 })
 
 local timer = vim.loop.new_timer()
 vim.api.nvim_create_autocmd("LspProgress", {
-  group = lsp_group,
-  callback = function()
-    vim.cmd.redrawstatus()
-    if timer then
-      timer:stop()
-      timer:start(500, 0, vim.schedule_wrap(function()
-        timer:stop()
-        vim.cmd.redrawstatus()
-      end))
-    end
-  end
+	group = lsp_group,
+	callback = function()
+		vim.cmd.redrawstatus()
+		if timer then
+			timer:stop()
+			timer:start(
+				500,
+				0,
+				vim.schedule_wrap(function()
+					timer:stop()
+					vim.cmd.redrawstatus()
+				end)
+			)
+		end
+	end,
 })
-
