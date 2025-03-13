92f734194b1dc46215838f8af9ceefa1b9c99df2
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-03-12 21:20:58 -0500

    feat: fish uses default fish theme with blue command and keyword

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -21,6 +21,7 @@
         fish_greeting = "test -e /etc/motd; and command cat /etc/motd";
         fish_user_key_bindings = "fish_default_key_bindings -M insert; fish_vi_key_bindings --no-erase insert";
       };
+      interactiveShellInit = "fish_config theme choose 'fish default'; set -g fish_color_command blue; set -g fish_color_keyword blue";
     };
     programs.bash.initExtra = lib.mkIf config.sebastianrasor.fish.bashInit ''
       if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
