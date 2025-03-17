b391044eda2eec34f2f4a66ff0f74c10acde85b9
Sebastian Rasor <git@rasor.us>
2025-03-16 23:46:34 -0500

    feat: add some ssh features

---
+++ modules/home-manager/ssh.nix
@@ -0,0 +1,15 @@
+{ config, lib, ... }:
+{
+  options = {
+    sebastianrasor.ssh.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.ssh.enable {
+    programs.ssh = {
+      enable = true;
+      controlMaster = "auto";
+      controlPersist = "10m";
+      matchBlocks."carbon.localdomain".forwardAgent = true;
+    };
+  };
+}
