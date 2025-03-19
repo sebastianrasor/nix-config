991104b087dc65883fe580303eb47f695a4d4f46
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-03-19 05:58:31 -0500

    feat: tmpfs root on azalea

--- configurations/azalea/default.nix
+++ configurations/azalea/default.nix
@@ -35,6 +35,12 @@
     fw-ectool
   ];
 
+  environment.etc."machine-id".source = "/nix/persist/etc/machine-id";
+  environment.etc."ssh/ssh_host_rsa_key".source = "/nix/persist/etc/ssh/ssh_host_rsa_key";
+  environment.etc."ssh/ssh_host_rsa_key.pub".source = "/nix/persist/etc/ssh/ssh_host_rsa_key.pub";
+  environment.etc."ssh/ssh_host_ed25519_key".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key";
+  environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/nix/persist/etc/ssh/ssh_host_ed25519_key.pub";
+
   imports =
     [
       nixos-hardware.nixosModules.framework-13-7040-amd
