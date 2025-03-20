4019302e478691845323046ba45c25ce6ba34440
Sebastian Rasor <git@rasor.us>
2025-03-19 23:42:37 -0500

    feat: make package an option for neovim home-manager module

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,11 +6,17 @@
   ...
 }: {
   options = {
-    sebastianrasor.neovim.enable = lib.mkEnableOption "";
+    sebastianrasor.neovim = {
+      enable = lib.mkEnableOption "";
+      package = lib.mkOption {
+        default = outputs.packages.${pkgs.system}.neovim-sebastianrasor;
+        type = lib.types.package;
+      };
+    };
   };
 
   config = lib.mkIf config.sebastianrasor.neovim.enable {
-    home.packages = [outputs.packages.${pkgs.system}.neovim-sebastianrasor];
+    home.packages = [config.sebastianrasor.neovim.package];
     programs.fish.shellAbbrs = lib.mkIf config.sebastianrasor.fish.enable {
       vim = "nvim";
     };
