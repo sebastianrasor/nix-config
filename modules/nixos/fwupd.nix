1f6865b33962f41e847c8aa3b33ad75b177b058a
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-03-23 16:23:12 -0500

    feat: add fwupd

---
+++ modules/nixos/fwupd.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    sebastianrasor.fwupd.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.fwupd.enable {
+    services.fwupd.enable = true;
+  };
+}
