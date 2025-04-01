9b085405f7cf8e0d9bbd59a721e9d2e3c29afea8
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-03-31 22:17:43 -0500

    chore: nix flake update and remove insecure package

--- modules/nixos/yubikey.nix
+++ modules/nixos/yubikey.nix
@@ -11,7 +11,6 @@
   config = lib.mkIf config.sebastianrasor.yubikey.enable {
     environment.systemPackages = with pkgs; [
       yubikey-manager
-      yubikey-manager-qt
       yubikey-personalization
       yubikey-personalization-gui
       yubico-piv-tool
