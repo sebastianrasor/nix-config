82f34affb923d85fb23e46456bb3c4b68727145b
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-04-01 10:51:15 -0500

    feat: add browserpass

---
+++ modules/home-manager/browserpass.nix
@@ -0,0 +1,13 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    sebastianrasor.browserpass.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.browserpass.enable {
+    programs.browserpass.enable = true;
+  };
+}
