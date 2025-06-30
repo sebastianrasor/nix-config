2de259767d27bf8d1a67e44ebece4a60bf8b559c
Sebastian Rasor <git@rasor.us>
2025-06-30 09:20:32 -0500

    feat: proxy_buffering off;

--- modules/nixos/jellyfin.nix
+++ modules/nixos/jellyfin.nix
@@ -54,7 +54,7 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:8096";
         proxyWebsockets = true;
-        extraConfig = "proxy_ssl_server_name on;" + "proxy_pass_header Authorization;";
+        extraConfig = "proxy_ssl_server_name on;" + "proxy_pass_header Authorization;" + "proxy_buffering off;";
       };
     };
   };
