42f8ce1840781a6635c91d778b2d37ab2b4782db
Sebastian Rasor <git@rasor.us>
2025-07-01 06:04:59 -0500

    feat: add monolith

---
+++ modules/home-manager/monolith.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    sebastianrasor.monolith.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.monolith.enable {
+    home.packages = [pkgs.monolith];
+  };
+}
