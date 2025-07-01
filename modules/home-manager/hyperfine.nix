d436419b714454b2e50dd68c2d971c787fefbdc9
Sebastian Rasor <git@rasor.us>
2025-07-01 05:55:15 -0500

    feat: add hyperfine

---
+++ modules/home-manager/hyperfine.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    sebastianrasor.hyperfine.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.hyperfine.enable {
+    home.packages = [pkgs.hyperfine];
+
+    programs.fish.shellAbbrs = lib.mkIf config.sebastianrasor.fish.enable {
+      hf = "hyperfine";
+    };
+  };
+}
