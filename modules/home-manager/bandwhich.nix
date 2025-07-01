273230488e61445b8b49c894a861a4e26bd287b9
Sebastian Rasor <git@rasor.us>
2025-07-01 06:11:21 -0500

    feat: add bandwhich

---
+++ modules/home-manager/bandwhich.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    sebastianrasor.bandwhich.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.bandwhich.enable {
+    home.packages = [pkgs.bandwhich];
+  };
+}
