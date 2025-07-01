11164497676a05070456dd2fefa8852dcb254d8b
Sebastian Rasor <git@rasor.us>
2025-07-01 07:27:06 -0500

    feat: add some abbreviations

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -19,8 +19,34 @@
       functions = {
         fish_greeting = "test -e /etc/motd; and command cat /etc/motd";
         fish_user_key_bindings = "fish_default_key_bindings -M insert; fish_vi_key_bindings --no-erase insert";
+        last_history_item = "echo $history[1]";
       };
       interactiveShellInit = "fish_config theme choose 'fish default'; set -g fish_color_command blue; set -g fish_color_keyword blue";
+      shellAbbrs = {
+        pgnc = {
+          expansion = "pass generate -nc % 43";
+          setCursor = true;
+        };
+        pgc = {
+          expansion = "pass generate -c % 39";
+          setCursor = true;
+        };
+        "!!" = {
+          function = "last_history_item";
+          position = "anywhere";
+        };
+        nix-search = {
+          expansion = "search nixpkgs";
+          regex = "search";
+          command = "nix";
+        };
+        nix-shell = {
+          expansion = "shell nixpkgs#% -c fish";
+          regex = "shell";
+          command = "nix";
+          setCursor = true;
+        };
+      };
     };
     programs.bash.initExtra = lib.mkIf config.sebastianrasor.fish.bashInit ''
       if [[ $(${lib.getExe' pkgs.procps "ps"} --no-header --pid=$PPID --format=comm) != "fish" && -z ''\${BASH_EXECUTION_STRING} && ''\${SHLVL} == 1 ]]; then
