a57d31616be3bb8df2c4ae47328c91d7239a653d
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-07-01 06:07:36 -0500

    feat: add gping

---
+++ modules/home-manager/gping.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    sebastianrasor.gping.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.gping.enable {
+    home.packages = [pkgs.gping];
+  };
+}
