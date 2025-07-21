33ac77be09f2db1fe9139909d0d80e7b998e0e6d
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-07-20 20:56:10 -0500

    feat: use posy cursors

---
+++ modules/home-manager/posy-cursors.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options = {
+    sebastianrasor.posy-cursors.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.posy-cursors.enable {
+    home.pointerCursor = {
+      package = pkgs.posy-cursors;
+      name = "Posy_Cursor_125_175";
+      size = 32;
+      gtk.enable = true;
+      x11.enable = true;
+    };
+  };
+}
