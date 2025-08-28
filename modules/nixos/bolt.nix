6e29a8e20bcb684122317db7b4f0bdf8960d0557
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-08-28 09:03:33 -0500

    feat: add bolt for thunderbolt support

---
+++ modules/nixos/bolt.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  lib,
+  ...
+}: {
+  options = {
+    sebastianrasor.bolt.enable = lib.mkEnableOption "";
+  };
+
+  config = lib.mkIf config.sebastianrasor.bolt.enable {
+    services.hardware.bolt.enable = true;
+    environment.persistence."${config.sebastianrasor.persistence.storagePath}".directories = lib.mkIf config.sebastianrasor.persistence.enable ["/var/lib/boltd"];
+  };
+}
