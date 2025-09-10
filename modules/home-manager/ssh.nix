eb30a119aee008b983545773a130d48f7422d71b
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-09-10 15:23:03 +0100

    feat: improve ssh behavior

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -11,14 +11,22 @@
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."*" = {
-        controlMaster = "auto";
-        controlPersist = "10m";
+      extraOptionOverrides = {
+        CanonicalizeHostname = "yes";
+        CanonicalDomains = "ts.rasor.us";
       };
-      matchBlocks.tailnet = {
-        match = "localnetwork 100.64.0.0/10";
+      matchBlocks."*.ts.${config.sebastianrasor.domain}" = {
+        controlMaster = "auto";
+        controlPath = "\${XDG_RUNTIME_DIR}/ssh/control/%C";
+        controlPersist = "10m";
         forwardAgent = true;
       };
     };
+    systemd.user.tmpfiles.rules = [
+      #Type  Path            Mode  User  Group  Age  Argument
+      "d     %t/ssh          0700  -     -      -    -"
+      "d     %t/ssh/control  0700  -     -      -    -"
+    ];
+    home.persistence."${config.sebastianrasor.persistence.storagePath}".files = lib.mkIf config.sebastianrasor.persistence.enable (builtins.map (lib.strings.removePrefix config.home.homeDirectory) ["${config.home.homeDirectory}/.ssh/known_hosts"]);
   };
 }
