1ffccef3f0d7495600ed2297b6cbaef8ad692018
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-10-15 09:33:13 +0100

    fix: add my user to networkmanager group

--- configurations/sunflower/users/sebastian/default.nix
+++ configurations/sunflower/users/sebastian/default.nix
@@ -7,7 +7,10 @@
     isNormalUser = true;
     home = "/home/sebastian";
     description = "Sebastian Rasor";
-    extraGroups = ["wheel"];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
     openssh.authorizedKeys.keys = lib.mkIf config.sebastianrasor.sshd.enable [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG71B1X8QTaPtldyB7UvST8bzYBLSyXHkKJG2BbT0tkG"
     ];
