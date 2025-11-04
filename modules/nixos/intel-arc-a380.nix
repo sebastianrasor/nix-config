41630af7586ebddd7ef530e957a792440d76a52a
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-11-04 15:37:10 -0600

    feat: improve arc a380 config

--- modules/nixos/intel-arc-a380.nix
+++ modules/nixos/intel-arc-a380.nix
@@ -9,14 +9,36 @@
   };
 
   config = lib.mkIf config.sebastianrasor.intel-arc-a380.enable {
-    hardware.graphics = {
-      enable = true;
-      extraPackages = with pkgs; [
-        intel-compute-runtime
-        intel-media-driver
-        intel-ocl
-        vpl-gpu-rt
-      ];
+    boot.kernelParams = ["i915.enable_guc=3"];
+
+    environment.sessionVariables = {
+      LIBVA_DRIVER_NAME = "iHD";
     };
+
+    hardware = {
+      enableAllFirmware = true;
+      enableRedistributableFirmware = true;
+      graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+          intel-media-driver # VA-API (iHD) userspace
+          vpl-gpu-rt # oneVPL (QSV) runtime
+
+          intel-compute-runtime # OpenCL (NEO) + Level Zero for Arc/Xe
+        ];
+      };
+    };
+
+    nixpkgs.overlays = [
+      (final: prev: {
+        jellyfin-ffmpeg = prev.jellyfin-ffmpeg.override {
+          ffmpeg_7-full = prev.ffmpeg_7-full.override {
+            withMfx = false;
+            withVpl = true;
+            withUnfree = true;
+          };
+        };
+      })
+    ];
   };
 }
