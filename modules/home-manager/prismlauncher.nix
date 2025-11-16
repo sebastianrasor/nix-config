37dabc68c8098c81d0ff7d7312693ca4da42ad91
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-11-15 23:10:48 -0600

    fix: persist correct prismlauncher directory

--- modules/home-manager/prismlauncher.nix
+++ modules/home-manager/prismlauncher.nix
@@ -10,6 +10,6 @@
 
   config = lib.mkIf config.sebastianrasor.prismlauncher.enable {
     home.packages = [pkgs.prismlauncher];
-    home.persistence."${config.sebastianrasor.persistence.storagePath}".directories = lib.mkIf config.sebastianrasor.persistence.enable (builtins.map (lib.strings.removePrefix config.home.homeDirectory) ["${config.xdg.stateHome}/PrismLauncher"]);
+    home.persistence."${config.sebastianrasor.persistence.storagePath}".directories = lib.mkIf config.sebastianrasor.persistence.enable (builtins.map (lib.strings.removePrefix config.home.homeDirectory) ["${config.xdg.dataHome}/PrismLauncher"]);
   };
 }
