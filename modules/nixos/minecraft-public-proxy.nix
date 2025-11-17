c9df265920b63edff3a45a1fcb7ddc324cc06b47
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2025-11-17 01:15:02 -0600

    fix: nginx stream for minecraft proxy needs same workaround for proxying to tailnet that authentik needs

--- modules/nixos/minecraft-public-proxy.nix
+++ modules/nixos/minecraft-public-proxy.nix
@@ -13,11 +13,15 @@
     services.nginx.streamConfig = ''
       server {
         listen 25565;
-        proxy_pass carbon.ts.${config.sebastianrasor.domain}:25565;
+        resolver 100.100.100.100;
+        set $carbon_mc "carbon.ts.${config.sebastianrasor.domain}:25565";
+        proxy_pass $carbon_mc;
       }
       server {
         listen 24454 udp;
-        proxy_pass carbon.ts.${config.sebastianrasor.domain}:24454;
+        resolver 100.100.100.100;
+        set $carbon_svc "carbon.ts.${config.sebastianrasor.domain}:24454";
+        proxy_pass $carbon_svc;
         proxy_responses 0;
       }
     '';
