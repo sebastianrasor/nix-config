84cd174b953273ec0e8b1e06ce6ffd78f8f6601b
Sebastian Rasor <git@rasor.us>
2025-12-01 16:23:28 -0600

    chore: nix flake update

--- modules/home-manager/pass.nix
+++ modules/home-manager/pass.nix
@@ -9,7 +9,10 @@
   };
 
   config = lib.mkIf config.sebastianrasor.pass.enable {
-    programs.password-store.enable = true;
+    programs.password-store = {
+      enable = true;
+      settings.PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
+    };
     sebastianrasor.persistence.directories = [
       config.programs.password-store.settings.PASSWORD_STORE_DIR
     ];
