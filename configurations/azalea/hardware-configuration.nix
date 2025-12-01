38ea159fe2b1a8ef749158bcb304bdde446681e7
Sebastian Rasor <git@rasor.us>
2025-12-01 15:38:05 -0600

    feat: use swapfile on azalea and nephele

--- configurations/azalea/hardware-configuration.nix
+++ configurations/azalea/hardware-configuration.nix
@@ -61,7 +61,12 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/nix/persist/swapfile";
+      size = 96 * 1024; # 96 GB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
