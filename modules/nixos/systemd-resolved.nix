32f7008649b16f3e31b519b2aa92c4129ea8bd47
Sebastian Rasor <git@rasor.us>
2026-02-01 10:09:43 -0600

    chore: nix flake update

--- modules/nixos/systemd-resolved.nix
+++ modules/nixos/systemd-resolved.nix
@@ -17,16 +17,16 @@ in
   config = lib.mkIf cfg.enable {
     services.resolved = {
       enable = true;
-      fallbackDns = [
-        "8.8.8.8"
-        "8.8.4.4"
-        "2001:4860:4860::8888"
-        "2001:4860:4860::8844"
-      ];
-      extraConfig = ''
-        MulticastDNS=yes
-        LLMNR=no
-      '';
+      settings.Resolve = {
+        fallbackDns = [
+          "8.8.8.8"
+          "8.8.4.4"
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
+        ];
+        MulticastDNS = true;
+        LLMR = false;
+      };
     };
   };
 }
