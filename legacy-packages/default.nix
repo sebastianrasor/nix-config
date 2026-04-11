4b56ea223f3e2aa3a002d993187daa890d053691
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2026-04-10 21:18:58 -0500

    feat: include legacyPackages in hydraJobs

--- legacy-packages/default.nix
+++ legacy-packages/default.nix
@@ -1,5 +1,5 @@
 { callPackages, ... }:
 rec {
   fabricmcServers = callPackages ./fabricmc-servers { };
-  fabricmc-server = fabricmcServers.fabricmc-server;
+  fabricmc-server-latest = fabricmcServers.fabricmc-server-latest;
 }
