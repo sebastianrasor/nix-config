007d49fb668b62219f067b862752423c00297c02
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2026-04-11 14:14:20 -0500

    fix: use writeShellScript instead of writeScript

--- hydra-jobs/run-command-hook/deploy.nix
+++ hydra-jobs/run-command-hook/deploy.nix
@@ -1,5 +1,5 @@
-{ writeScript, ... }:
-writeScript "deploy-server" ''
+{ writeShellScript, ... }:
+writeShellScript "deploy-server" ''
   if [ -f /run/deploy-server.stdin ]; then
     echo switch > /run/deploy-server.stdin
   fi
