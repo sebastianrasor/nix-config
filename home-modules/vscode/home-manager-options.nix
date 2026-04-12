5777e336dd0f2e57ffe0dc644dff62a703c7a680
Sebastian Rasor <git@rasor.us>
2026-04-12 01:10:17 -0500

    feat: add vscode & remove nvf from core

---
+++ home-modules/vscode/home-manager-options.nix
@@ -0,0 +1,24 @@
+let
+  flake = builtins.getFlake (toString ./.);
+  pkgs = import flake.inputs.nixpkgs { };
+in
+(flake.inputs.home-manager.lib.homeManagerConfiguration {
+  inherit pkgs;
+  extraSpecialArgs = { inherit (flake) inputs; };
+  check = false;
+  modules = [
+    {
+      home = {
+        homeDirectory = "/home/sebastian";
+        username = "sebastian";
+        stateVersion =
+          pkgs.lib.lists.last
+            (import (flake.inputs.home-manager.outPath + "/modules/misc/version.nix") {
+              config = { };
+              inherit (pkgs) lib;
+            }).options.home.stateVersion.type.functor.payload.values;
+      };
+    }
+  ]
+  ++ builtins.attrValues (flake.outputs.homeModules or { });
+}).options
