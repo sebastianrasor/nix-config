129f031d913726ee091dbd66588473fc280fc37d
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2026-04-12 02:33:21 -0500

    feat: add githubstatus hydra plugin

--- nixos-modules/hydra.nix
+++ nixos-modules/hydra.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.sebastianrasor.hydra;
+  secretsEnabled = config.sebastianrasor.secrets.enable;
 in
 {
   options.sebastianrasor.hydra = {
@@ -23,13 +24,23 @@ in
       notificationSender = "hydra@${constants.domain}";
       useSubstitutes = true;
       extraConfig = ''
+        Import ${config.sops.secrets."hydra-github-authorization".path}
         <dynamicruncommand>
           enable = 1
         </dynamicruncommand>
+        <githubstatus>
+          jobs = nix-config:nix-config:.*
+          useShortContext = true
+        </githubstatus>
       '';
     };
 
     sebastianrasor.reverse-proxy.proxies."hydra" =
       "http://${config.services.hydra.listenHost}:${toString config.services.hydra.port}";
+
+    sops.secrets."hydra-github-authorization" = lib.mkIf secretsEnabled {
+      owner = lib.mkDefault config.users.users.gate.name;
+      group = lib.mkDefault config.users.users.gate.group;
+    };
   };
 }
