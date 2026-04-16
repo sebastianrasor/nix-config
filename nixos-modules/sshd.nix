a442fdc2e0b8cfe32e3dd6431c6865fe821b3933
Sebastian Rasor <https://www.sebastianrasor.com/contact>
2026-04-16 05:04:07 -0500

    feat: allow root logins for deploy

--- nixos-modules/sshd.nix
+++ nixos-modules/sshd.nix
@@ -42,7 +42,7 @@ in
       settings = {
         KbdInteractiveAuthentication = false;
         PasswordAuthentication = false;
-        PermitRootLogin = "no";
+        PermitRootLogin = "prohibit-password";
         UseDns = true;
       };
     };
