b824f69f52fa1956f3f4f4bd2f10ddb7b55673cd
Sebastian Rasor <92653912+sebastianrasor@users.noreply.github.com>
2026-04-18 16:41:33 -0500

    style: extract devshells definition into a separate file

--- flake.nix
+++ flake.nix
@@ -145,29 +145,7 @@
         };
       };
 
-      devShells = forAllSystems (
-        pkgs:
-        let
-          lib = pkgs.lib;
-          updateName = nameUpdater: x: x // { name = nameUpdater x.name; };
-        in
-        (lib.pipe ./. [
-          lib.filesystem.listFilesRecursive
-          (lib.filter (file: baseNameOf file == "shell.nix"))
-          (map (shell: {
-            name = lib.path.removePrefix ./. shell;
-            value = import shell { inherit pkgs; };
-          }))
-          (map (updateName (lib.path.subpath.components)))
-          (map (updateName (lib.init)))
-          (lib.filter (attrs: attrs.name != [ ]))
-          (map (updateName (lib.join "/")))
-          builtins.listToAttrs
-        ])
-        // {
-          default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+      devShells = forAllSystems (pkgs: import ./devshells.nix pkgs);
 
       formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
 
