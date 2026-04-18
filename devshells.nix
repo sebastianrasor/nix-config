fb55257fa5e6473632b5ba63a75c852177676f17
Sebastian Rasor <git@rasor.us>
2026-04-18 16:41:33 -0500

    style: extract devshells definition into a separate file

---
+++ devshells.nix
@@ -0,0 +1,21 @@
+pkgs:
+let
+  lib = pkgs.lib;
+  updateName = nameUpdater: x: x // { name = nameUpdater x.name; };
+in
+(lib.pipe ./. [
+  lib.filesystem.listFilesRecursive
+  (lib.filter (file: baseNameOf file == "shell.nix"))
+  (map (shell: {
+    name = lib.path.removePrefix ./. shell;
+    value = import shell { inherit pkgs; };
+  }))
+  (map (updateName (lib.path.subpath.components)))
+  (map (updateName (lib.init)))
+  (lib.filter (attrs: attrs.name != [ ]))
+  (map (updateName (lib.join "/")))
+  builtins.listToAttrs
+])
+// {
+  default = import ./shell.nix { inherit pkgs; };
+}
