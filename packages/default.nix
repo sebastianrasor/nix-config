eb7c105ddc896181b7cb26094b14f8e9089ed8ab
Sebastian Rasor <git@rasor.us>
2026-04-19 20:05:53 -0500

    feat: only output packages for platforms the package is available on

--- packages/default.nix
+++ packages/default.nix
@@ -1,11 +1,15 @@
 pkgs:
-pkgs.lib.pipe ./. [
+let
+  inherit (pkgs) lib;
+in
+lib.pipe ./. [
   builtins.readDir
   builtins.attrNames
   (builtins.filter (name: name != "default.nix"))
   (map (name: {
-    name = pkgs.lib.removeSuffix ".nix" name;
+    name = lib.removeSuffix ".nix" name;
     value = pkgs.callPackage ./${name} { };
   }))
   builtins.listToAttrs
+  (lib.filterAttrs (_: lib.meta.availableOn { inherit (pkgs.stdenv.hostPlatform) system; }))
 ]
