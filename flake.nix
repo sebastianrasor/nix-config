8341bbf76b77830a46d54b28971f8781f05600d2
Sebastian Rasor <git@rasor.us>
2026-04-19 19:06:22 -0500

    feat: extract effects definition into its own file

--- flake.nix
+++ flake.nix
@@ -4,13 +4,11 @@
   outputs =
     inputs@{
       self,
-      hercules-ci-effects,
       nixpkgs,
       ...
     }:
     let
       constants = import ./constants.nix;
-      inherit (constants._module.args.constants) domain;
       supportedSystems = [
         "x86_64-linux"
       ];
@@ -74,80 +72,12 @@
           packages
         ];
 
-      herculesCI = hercules-ci-effects.lib.mkHerculesCI { inherit inputs; } {
-        herculesCI = herculesCI: {
-          onPush.default.outputs.effects = nixpkgs.lib.pipe self.nixosConfigurations [
-            builtins.attrValues
-            (map (
-              nixosConfiguration:
-              let
-                inherit (nixosConfiguration.config.networking) hostName;
-                inherit (nixosConfiguration.config.system.build.toplevel) outPath;
-                pkgs = import nixpkgs {
-                  inherit (nixosConfiguration.config.nixpkgs.hostPlatform) system;
-                };
-                hci-effects = hercules-ci-effects.lib.withPkgs pkgs;
-                sshCommand =
-                  hci-effects.ssh
-                    {
-                      destination = "${hostName}.ts.${domain}";
-                      sshOptions = "-o ConnectTimeout=10";
-                    }
-                    ''
-                      if [ "$(readlink -f /run/current-system)" == "${outPath}" ]; then
-                        exit 0
-                      fi
-
-                      cmd=(
-                        "systemd-run"
-                        "-E" "LOCALE_ARCHIVE"
-                        "--collect"
-                        "--no-ask-password"
-                        "--pty"
-                        "--quiet"
-                        "--same-dir"
-                        "--service-type=exec"
-                        "--unit=deploy-switch-to-configuration"
-                        "--wait"
-                        "${outPath}/bin/switch-to-configuration"
-                      )
-
-                      nix-env -p /nix/var/nix/profiles/system --set ${outPath}
-                      "''${cmd[@]}" switch
-                    '';
-              in
-              {
-                name = "deploy-${hostName}";
-                value = hci-effects.runIf (herculesCI.config.repo.branch == "main") (
-                  hci-effects.mkEffect {
-                    effectScript = ''
-                      writeSSHKey ssh
-                      # todo: move this into the configuration itself somehow
-                      cat >~/.ssh/known_hosts <<EOF
-                      azalea.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5pVGXbupdNG/9acDwOd6loG8CBBaNsreyoYCY4at9J
-                      carbon.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGdZ/fyPx7w2GsbE337H0kNst+GWL/gN4piJizkWj/9
-                      nephele.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISaqWRMez2mFczqhMmiYe0KzNeENKsqEQw/AsOC+Ay+
-                      sunflower.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII85WkAO+BoPgHC8Vj7Y3ab3aOOLDx9e8jul4rBLAXiM
-                      EOF
-
-                      if ${sshCommand}; then
-                        exit "''${?/255/0}"
-                      fi
-
-                      exit 0
-                    '';
-                  }
-                );
-              }
-            ))
-            builtins.listToAttrs
-          ];
-        };
-      };
 
       devShells = forAllSystems (pkgs: import ./devshells.nix pkgs);
 
       formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+
+      herculesCI = import ./hercules-ci.nix inputs;
 
       homeModules = import ./home-modules inputs // {
         inherit constants;
