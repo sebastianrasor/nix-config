a55a5c05570659de8754e2b02844caa52a4621fb
Sebastian Rasor <git@rasor.us>
2026-04-19 20:25:53 -0500

    feat: only output legacy packages for platforms they're available on

--- legacy-packages/default.nix
+++ legacy-packages/default.nix
@@ -1,5 +1,18 @@
-{ callPackages, ... }:
-rec {
-  fabricmcServers = callPackages ./fabricmc-servers { };
-  inherit (fabricmcServers) fabricmc-server-latest;
-}
+pkgs:
+let
+  inherit (pkgs) lib stdenv;
+in
+lib.filterAttrsRecursive
+  (
+    _: v:
+    let
+      tryAvailableOn = builtins.tryEval (
+        lib.meta.availableOn { inherit (stdenv.hostPlatform) system; } v
+      );
+    in
+    !(lib.isDerivation v) || (tryAvailableOn.success && tryAvailableOn.value)
+  )
+  rec {
+    fabricmcServers = pkgs.callPackages ./fabricmc-servers { };
+    inherit (fabricmcServers) fabricmc-server-latest;
+  }
