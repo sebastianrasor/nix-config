e34b6334028f2c4eb0648b7e9b6d113810ccd221
Sebastian Rasor <git@rasor.us>
2026-06-16 05:42:05 -0500

    Enable framework audio enhancements on azalea

--- nixos-configurations/azalea/hardware-configuration.nix
+++ nixos-configurations/azalea/hardware-configuration.nix
@@ -70,4 +70,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.framework.laptop13.audioEnhancement.enable = true;
 }
