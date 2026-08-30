acbafe51f07db8c4a554c8d410cd9b77083edebe
Sebastian Rasor <git@rasor.us>
2026-08-29 19:47:27 -0500

    Add sakura SSH host key

--- hercules-ci.nix
+++ hercules-ci.nix
@@ -48,6 +48,7 @@ hercules-ci-effects.lib.mkHerculesCI { inherit inputs; } {
                 azalea.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5pVGXbupdNG/9acDwOd6loG8CBBaNsreyoYCY4at9J
                 carbon.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAGdZ/fyPx7w2GsbE337H0kNst+GWL/gN4piJizkWj/9
                 nephele.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIISaqWRMez2mFczqhMmiYe0KzNeENKsqEQw/AsOC+Ay+
+                sakura.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdHZOOgrwMkv66j61MiTU3oHacuTUW9+SL3r63P/3Ww
                 sunflower.ts.${domain} ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII85WkAO+BoPgHC8Vj7Y3ab3aOOLDx9e8jul4rBLAXiM
                 EOF
 
